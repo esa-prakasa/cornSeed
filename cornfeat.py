c91757e12289aa3aab9a26eebb8cd9aa3d49adcd
Esa Prakasa <esa.prakasa@gmail.com>
2020-05-07 00:09:04 +0700

    Add files via upload

--- cornfeat.py
+++ cornfeat.py
@@ -1,6 +1,7 @@
 import cv2
 import os
 import numpy as np
+import math
 
 os.system("cls")
 
@@ -15,16 +16,83 @@ def getAvgIntensity(img, i0, j0, fSz):
 	return avgVal
 
 
+def getCentroidOfMass(img):
+	M = img.shape[0]
+	N = img.shape[1]
+	iC = 0
+	jC = 0
+	Nimg = 0
+	for i in range(M):
+		for j in range(N):
+			if (img[i,j] == 255):
+				Nimg = Nimg + 1
+				iC = iC + i
+				jC = jC + j
+	iC = int(iC/Nimg)
+	jC = int(jC/Nimg)
+	return iC,jC
 
 
-path = "C:\\Users\\INKOM06\\Pictures\\jagung2020\\largeDataSet\\"
+def getRadiusAndAngle(i,j,iC,jC):
+
+	dI = i - iC
+	dJ = j - jC
+
+
+
+	if ((dI==0)&(dJ>0)):  #Position at 0 deg
+		R = dJ
+		deg = 0
+
+	if ((dI<0)&(dJ==0)):  #Position at 90 deg
+		R = abs(dI)
+		deg = 90
+
+	if ((dI==0)&(dJ<0)):  #Position at 180 deg
+		R = abs(dJ)
+		deg = 180
+
+	if ((dI>0)&(dJ==0)):  #Position at 270 deg
+		R = abs(dI)
+		deg = 270
+
+	if ((dI!=0)&(dJ!=0)):
+
+		R = math.sqrt((dI**2)+(dJ**2))
+		deg = abs(math.atan(dI/dJ)*180/3.14)
+
+		if ((dI<0)&(dJ>0)):
+			deg = deg
+
+		if ((dI<0)&(dJ<0)):
+			deg = 180 - deg
+
+		if ((dI>0)&(dJ<0)):
+			deg = 180 + deg
+
+		if ((dI>0)&(dJ>0)):
+			deg = 360 - deg
+
+	return R, deg 
+
+
+
+
+
+
+
+path = "C:\\Users\\INKOM06\\Pictures\\jagung2020\\largeDataSet\\true\\"
+path = "C:\\Users\\INKOM06\\Pictures\\jagung2020\\largeDataSet\\model\\"
+path = "C:\\Users\\INKOM06\\Pictures\\jagung2020\\largeDataSet\\false\\"
+
+#path = "C:\\Users\\INKOM06\\Pictures\\jagung2020\\largeDataSet\\"
 
 fileList = []
 with os.scandir(path) as entries:
     for entry in entries:
         fileList.append(entry.name)
 
-for i in range (1,11,1):
+for i in range (1,5,1):
 	print(str(i)+"  "+fileList[i])
 
 
@@ -36,7 +104,7 @@ N = img.shape[1]
 ratio = 0.5
 
 img = cv2.resize(img,(int(ratio*M), int(ratio*N)) , interpolation = cv2.INTER_AREA)   
-cv2.imshow("RED",img[:,:,2])
+#cv2.imshow("RED",img[:,:,2])
 #cv2.imshow("GREEN",img[:,:,1])
 #cv2.imshow("BLUE",img[:,:,0])
 
@@ -67,11 +135,119 @@ print(finalAvg)
 
 
 thr,imgBW = cv2.threshold(img[:,:,2],int(finalAvg*5),255,cv2.THRESH_BINARY)
-cv2.imshow("imgBW",imgBW)
+#cv2.imshow("imgBW",imgBW)
+
+
+centPt = [0, 0]
+centPt = getCentroidOfMass(imgBW)
+
+
+imgBWrgb = np.zeros((M,N,3), dtype=np.uint8)
+for i in range(3):
+	imgBWrgb[:,:,i] = imgBW
+
+print(centPt[0])
+print(centPt[1])
+
+
+for k in range(-3,4,1):
+	for l in range(-3,4,1):
+		imgBWrgb[ (centPt[0]+k), (centPt[1]+l), 0] = 0
+		imgBWrgb[ (centPt[0]+k), (centPt[1]+l), 1] = 0
+		imgBWrgb[ (centPt[0]+k), (centPt[1]+l), 2] = 255
+
+
+# Canny Edge Detection
+edges = cv2.Canny(imgBW,100,200)
+#cv2.imshow("Edges image", edges)
+
+
+#Get dataPoint R(delta)
+iC = centPt[0]
+jC = centPt[1]
+phi = 3.14
+NPt = 0 
+
+radArr = []
+degArr  = []
+#R = 0
+#deg = 0
+
+for i in range(M):
+	for j in range(N):
+		if edges[i,j] == 255:
+			NPt = NPt + 1
+			#print(NPt)
+			[R, deg] = getRadiusAndAngle(i,j,iC,jC)
+
+			radArr.append(R)			
+			degArr.append(deg)
+
+
+
+
+
+
+
+
+#cv2.imshow("imgBW RGB",imgBWrgb)
+
 
 #kernel = np.ones((5,5), np.uint8) 
 #imgEro = cv2.erode(imgBW, kernel, iterations=1) 
 #cv2.imshow("img Erroded",imgEro)
+
+
+import matplotlib.pyplot as plt
+
+
+fig, axs = plt.subplots(1,2,figsize=(10,4))
+#fig= plt.figure(figsize=(600,300))
+
+fig.suptitle("Profile extraction of image "+str(imgIdx)+": "+fileList[imgIdx])
+axs[0].set_title('Segmented image')
+axs[0].imshow(imgBW, cmap='gray', vmin=0, vmax=255)
+
+yMax = max(M,N)
+
+axs[1].set_title('Boundary profile: Radius vs. Degree')
+axs[1].axis([0, yMax, 0, 300])
+axs[1].plot(degArr,radArr, 'r.')
+
+#plt.show()
+
+
+pathToSave = "C:\\Users\\INKOM06\\Documents\\[0--KEGIATAN-Ku-2020\\2020.02-011-IDENTIFIKASI Jagung 2020\\Eksperimen Feature Extraction\\figures\\"
+
+figPath = pathToSave+"fig_"+fileList[imgIdx]
+print(figPath)
+fig.savefig(figPath, dpi=fig.dpi)
+
+
+
+
+'''
+plt.figure(figsize=(9, 3))
+
+plt.imshow(imgBW)
+plt.plot(degArr,radArr, 'ro')
+plt.subplot(122)
+plt.plot(degArr,radArr, 'ro')
+plt.suptitle('Categorical Plotting')
+plt.show()
+'''
+
+
+#plt.axis([0, M, 0, 20])
+#plt.show()
+
+
+
+
+#plt.plot([1, 2, 3, 4])
+#plt.ylabel('some numbers')
+#plt.show()
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
